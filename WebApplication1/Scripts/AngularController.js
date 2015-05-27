cff548a58b62fa372e93295280c55ab812ba5035
Svetoslav <dev@pvp-gaming.net>
2015-05-27 22:45:24 +0300

    Login hooked to angular and will swap the main page upon login

--- WebApplication1/Scripts/AngularController.js
+++ WebApplication1/Scripts/AngularController.js
@@ -6,5 +6,27 @@ socialNetwork.config(function ($routeProvider, $locationProvider) {
 });
 
 socialNetwork.controller("SocialNetworkController", function($scope) {
-    
+    Init();
+
+    function Init() {
+        var token = getCookie("access_token");
+        $scope.isLogedIn = (token != "");
+    }
+
+    $scope.tryLogin = function() {
+        var username = $("#login-username").val();
+        var password = $("#login-password").val();
+        $.post(backEndURL + "/api/users/Login", { Username: username, Password: password }, function (data) {
+            if (typeof (data) !== undefined) {
+                //console.log(new Date(data.expires_in));
+                document.cookie = "access_token=" + data.access_token;
+                //console.log(document.cookie);
+                $scope.isLogedIn = true;
+                $scope.$apply();
+            }
+        }).fail(function () {
+            $scope.loginErrorMessage = "Wrong username or password!";
+            $scope.$apply();
+        });
+    }
 });
--- WebApplication1/Scripts/LoginForm.js
+++ WebApplication1/Scripts/LoginForm.js
@@ -1,16 +1,4 @@
-function tryLogin() {
-    var username = $("#login-username").val();
-    var password = $("#login-password").val();
-    $.post(backEndURL + "/api/users/Login", { Username: username, Password: password }, function (data) {
-        if (typeof (data) !== undefined) {
-            //console.log(new Date(data.expires_in));
-            document.cookie = "access_token=" + data.access_token;
-            //console.log(document.cookie);
-        }
-    }).fail(function () {
-        $("#LoginErrorMessage").text("Wrong username or password!");
-    });
-}
+
 
 $('#registerForm')
     .ajaxForm({
